e8d92ffd9cf71f4bf515c30f1ab6d46fac09442d
akilamaithri <akilamaithripala@gmail.com>
2022-08-11 10:44:16 +0530

    Interview error correction

--- frontend/src/pages/Student/StudentInterviews.js
+++ frontend/src/pages/Student/StudentInterviews.js
@@ -8,34 +8,6 @@ import Card from 'react-bootstrap/Card';
 import { Accordion } from 'react-bootstrap';
 import AccordionItem from '../../component/Accordion/accordion';
 
-const StudentInterviews = () => {  
-return (
-  <Container>
-      <Form>
-        <h2>Interviews</h2><br />
-        
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-    </Form>
-  </Container>
-
-
 function StudentInterviews() {
   const [value, onChange] = useState(new Date());
 
