2d0e0ad55b379994f1b6a7a83756b09899392b7d
akilamaithri <akilamaithripala@gmail.com>
2022-08-21 14:35:40 +0530

    accordion update

--- frontend/src/component/Accordion/accordion.js
+++ frontend/src/component/Accordion/accordion.js
@@ -1,20 +1,73 @@
 import Accordion from 'react-bootstrap/Accordion';
 import InfoBtn from '../Buttons/outlineBtn';
 import PrimaryBtn from '../Buttons/primaryBtn';
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function AccordionItem(props) {
   return (
-    <Accordion defaultActiveKey={['0']} alwaysOpen>
+    <Accordion >
         
       <Accordion.Item eventKey="0">
         <Accordion.Header>{props.Header}</Accordion.Header>
         <Accordion.Body>
           {props.body}
           <br /><br />
+
+          
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }} className='mb-4'>
+                <Card.Header>{props.card1heading}</Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{props.card1context}</ListGroup.Item>
+                </ListGroup>
+              </Card>   
+            </Col>
+
+            <Col>
+              <Card style={{ width: '18rem' }} className='mb-4'>
+                <Card.Header>{props.card2heading}</Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{props.card2context}</ListGroup.Item>
+                </ListGroup>
+              </Card>   
+            </Col>
+
+          </Row>
+
+
+          <Row>
+            <Col>
+              <Card style={{ width: '18rem' }} className='mb-4'>
+                <Card.Header>{props.card3heading}</Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{props.card3context}</ListGroup.Item>
+
+                </ListGroup>
+              </Card>   
+            </Col>
+
+            <Col>
+              <Card style={{ width: '18rem' }} className='mb-4'>
+                <Card.Header>{props.card4heading}</Card.Header>
+                <ListGroup variant="flush">
+                  <ListGroup.Item>{props.card4context}</ListGroup.Item>
+
+                </ListGroup>
+              </Card>   
+            </Col>
+
+          </Row>
+          
+          
+
           <div style={{marginRight: '5px', display: 'inline'}}>
-          <PrimaryBtn text='Accept' ></PrimaryBtn>
+          <PrimaryBtn text={props.PrimaryBtn} onClick={props.PrimaryBtnLink}></PrimaryBtn>
           </div>
-          <InfoBtn text='View'></InfoBtn>
+          <InfoBtn text={props.InfoBtn} onClick={props.InfoBtnLink}></InfoBtn>
 
         </Accordion.Body>
 
