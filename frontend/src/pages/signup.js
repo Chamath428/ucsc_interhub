219194c395531161a28eda060164e7adee23affa
Chamath Chinthana <62209498+Chamath428@users.noreply.github.com>
2022-08-23 01:19:10 +0530

    register-organization-api-mapping

--- frontend/src/pages/signup.js
+++ frontend/src/pages/signup.js
@@ -3,13 +3,41 @@ import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import React, { useState } from 'react';
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import Alert from 'react-bootstrap/Alert';
+import DashboardButton from '../component/Dashboard/DashboardButton/dashboardButton';
+import axios from 'axios';
+import { URL } from './URL';
 import '../styles/Signup.css';
-import PrimaryButton from '../component/homepage/PrimaryButton/primaryButtonClass';
 
 function Signup() {
 
     const [validated, setValidated] = useState(false);
+    const [name,setName] = useState("");
+    const [stablishedDate,setStablishedDate] = useState("");
+    const [registerationNumber,setRegisterationNumber] = useState("");
+    const [address,setAddress] = useState("");
+    const [number,setNumber] = useState();
+    const [faxNumber,setFaxNumber] = useState();
+    const [partners,setpartners] = useState("");
+    const [clients,setClients] = useState("");
+    const [companyLink,setCompanyLink] = useState("");
+    const [details,setDetails] = useState("");
+    const [primaryContactName,setPrimaryContactName] = useState("");
+    const [email,setEmail] = useState("");
+    const [landTelephone,setLandTelephone] = useState();
+    const [mobiTelephone,setMobiTelephone] = useState();
+    const [numberOfEmployees,setNumberOfEmployees] = useState();
+    const [numberOfProjectManagers,setNumberOfProjectManagers] = useState();
+    const [numberOfTechLeads,setNumberOfTechLeads] = useState();
+    const [username,setUserName] = useState("");
+    const [password,setPassword] = useState("");
+    const [confirmPassword,setConfirmPassword] = useState("");
+    const [show, setShow] = useState(false);
+    const [alertPara,setAlertPara] = useState("Organization Registered Successfully!");
+    const [variant,setVariant] = useState("success");
+
+    const [isRegisteredOtherUni,setIsRegisteredOtherUni] = useState(1);
+
 
     const handleSubmit = (event) => {
         const form = event.currentTarget;
@@ -21,22 +49,64 @@ function Signup() {
         setValidated(true);
     };
 
+    const registerOrganization = (event)=>{
+        event.preventDefault();
+        axios.post(`${URL}/organization/register`,{
+            name:name,
+            registration_number:registerationNumber,
+            date_of_establishment:stablishedDate,  
+            address:address,
+            telephone_no:parseInt(number),
+            fax_no:parseInt(faxNumber),
+            partners:partners,
+            client:clients,
+            website:companyLink,
+            description:details,
+            primaryContactName:primaryContactName,  //this is the name of the hr in the db
+            email:email,
+            mobile_no:parseInt(mobiTelephone),
+            landline_no:parseInt(landTelephone),
+            no_of_employees:parseInt(numberOfEmployees),
+            no_of_project_managers:parseInt(numberOfProjectManagers),
+            no_of_tech_leads:parseInt(numberOfTechLeads),
+            username:username,
+            password:password,
+            confirm_password:confirmPassword,
+            is_registered_other_uni:parseInt(isRegisteredOtherUni)
+        }).then((response)=>{showAlert(response)}).catch(function (error) {
+            if (error.response) {
+              setAlertPara("Something went wrong when registering the organization!");
+              setVariant("danger");
+              setShow(true);
+            }})
+    }
+
+    const showAlert=(response)=>{
+        setAlertPara("Organization Registered Successfully!");
+        setVariant("success");
+        setShow(true);
+       }
+
   return (
 <Container className="shadow p-4 bg-body rounded form-container">
 
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+      <Alert variant={variant} show={show} onClose={() => setShow(false)} dismissible>
+        <Alert.Heading>{alertPara}</Alert.Heading>
+      </Alert>
+
+    <Form noValidate validated={validated} onSubmit={registerOrganization}>
 
         <Row className="justify-content-md-between">
             <h2>Register as an Organizations/Company</h2>
             <Col lg={6} className="mt-5">
                 <h4 className='mb-4'>Details of the Company</h4>
-
                 <Row className="mb-3">
                     <Form.Group as={Col} xl="6" controlId="validationCustom01">
                         <Form.Label>Organization/Company Name</Form.Label>
                         <Form.Control
                             required
                             type="text"
+                            onChange={(event)=>{setName(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide a name.
@@ -48,6 +118,7 @@ function Signup() {
                         <Form.Control
                             required
                             type="date"
+                            onChange={(event)=>{setStablishedDate(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide the date of stablishment.
@@ -61,6 +132,7 @@ function Signup() {
                         <Form.Control
                             required
                             type="text"
+                            onChange={(event)=>{setRegisterationNumber(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide the registration number.
@@ -72,6 +144,7 @@ function Signup() {
                         <Form.Control
                             required
                             type="text"
+                            onChange={(event)=>{setAddress(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide the current address.
@@ -85,6 +158,7 @@ function Signup() {
                         <Form.Control
                             required
                             type="number"
+                            onChange={(event)=>{setNumber(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide the telephone number.
@@ -95,6 +169,7 @@ function Signup() {
                     <Form.Label>Fax Number</Form.Label>
                     <Form.Control
                         type="text"
+                        onChange={(event)=>{setFaxNumber(event.target.value)}}
                     />
                     </Form.Group>
                 </Row>
@@ -104,6 +179,7 @@ function Signup() {
                     <Form.Label>Partners (If any)</Form.Label>
                     <Form.Control
                         type="text"
+                        onChange={(event)=>{setpartners(event.target.value)}}
                     />
                     </Form.Group>
 
@@ -111,6 +187,7 @@ function Signup() {
                     <Form.Label>Clients (Optional)</Form.Label>
                     <Form.Control
                         type="text"
+                        onChange={(event)=>{setClients(event.target.value)}}
                     />
                     </Form.Group>
                 </Row>
@@ -121,6 +198,7 @@ function Signup() {
                         <Form.Control
                             required
                             type="text"
+                            onChange={(event)=>{setCompanyLink(event.target.value)}}
                         />
                         <Form.Control.Feedback type="invalid">
                             Please provide the website link.
@@ -131,14 +209,15 @@ function Signup() {
                 <Row className="mb-3">
                     <Form.Group as={Col} xl="12" className="mb-3" controlId="exampleForm.ControlTextarea1">
                         <Form.Label>Organization/Company Details</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
+                        <Form.Control 
+                        as="textarea" 
+                        rows={3} 
+                        onChange={(event)=>{setDetails(event.target.value)}}
+                        />
                     </Form.Group>
                 </Row>
 
-            </Col>
-
-            <Col lg={6} className="mt-5">
-
+                
                 <Row className="mb-4">
                     <h4 className='mb-4'>Details of the Main Contact</h4>
                     <Row className="mb-3">
@@ -147,6 +226,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="text"
+                                onChange={(event)=>{setPrimaryContactName(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the primary contact name.
@@ -158,6 +238,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="text"
+                                onChange={(event)=>{setEmail(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the email.
@@ -169,8 +250,8 @@ function Signup() {
                         <Form.Group as={Col} xl="6" controlId="validationCustom01">
                             <Form.Label>Telephone No (Landline)</Form.Label>
                             <Form.Control
-                                required
                                 type="number"
+                                onChange={(event)=>{setLandTelephone(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the telephone number (Landline).
@@ -182,6 +263,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="number"
+                                onChange={(event)=>{setMobiTelephone(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the Telephone No (Mobile).
@@ -191,6 +273,9 @@ function Signup() {
 
                 </Row>
 
+            </Col>
+
+            <Col lg={6} className="mt-5">
                 <Row>
                     <h4 className='mb-4'>Employee Hierarchy</h4>
                     <Row className="mb-3">
@@ -199,6 +284,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="number"
+                                onChange={(event)=>{setNumberOfEmployees(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the number of employees.
@@ -210,6 +296,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="number"
+                                onChange={(event)=>{setNumberOfProjectManagers(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the number of project managers.
@@ -223,6 +310,7 @@ function Signup() {
                             <Form.Control
                                 required
                                 type="number"
+                                onChange={(event)=>{setNumberOfTechLeads(event.target.value)}}
                             />
                             <Form.Control.Feedback type="invalid">
                                 Please provide the number of tech leads.
@@ -230,7 +318,55 @@ function Signup() {
                         </Form.Group>
                     </Row>
 
+                   
+                </Row>
+                
+                {/* User details starts here */}
+                
+                <Row className="mb-4">
+                    <h4 className='mb-4'>User Details</h4>
+
                     <Row className="mb-3">
+                    <Form.Group as={Col} xl="6" controlId="validationCustom01">
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control
+                            required
+                            type="text"
+                            onChange={(event)=>{setUserName(event.target.value)}}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a username.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+
+                    <Form.Group as={Col} xl="6" controlId="validationCustom02">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control
+                            required
+                            type="password"
+                            onChange={(event)=>{setPassword(event.target.value)}}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a password.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
+                    <Form.Group as={Col} xl="6" controlId="validationCustom02">
+                        <Form.Label>Confirm Password</Form.Label>
+                        <Form.Control
+                            required
+                            type="password"
+                            onChange={(event)=>{setConfirmPassword(event.target.value)}}
+                        />
+                        <Form.Control.Feedback type="invalid">
+                            Please provide a confirm password.
+                        </Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+
+                <Row className="mb-3">
                         <Form.Group as={Col} xl="12" controlId="validationCustom01">
                             <Form.Label className="fw-bold">Specify whether the company/organization has been offered industry placement for any other universities/educational institutes before. </Form.Label>
                             {[ 'radio'].map((type) => (
@@ -242,6 +378,7 @@ function Signup() {
                                     type={type}
                                     id={`inline-${type}-1`}
                                     checked
+                                    onChange={(event)=>{setIsRegisteredOtherUni(1)}}
                                 />
                                 <Form.Check
                                     inline
@@ -249,6 +386,7 @@ function Signup() {
                                     name="group1"
                                     type={type}
                                     id={`inline-${type}-2`}
+                                    onChange={(event)=>{setIsRegisteredOtherUni(0)}}
                                 />
                                 </div>
                             ))}
@@ -257,11 +395,14 @@ function Signup() {
 
                     <Row className="mb-3" >
                         <div className='d-flex flex-row-reverse'>
-                            <PrimaryButton inside={'Submit'}></PrimaryButton>
+                            <DashboardButton inside={'Submit'}></DashboardButton>
                         </div>
                     </Row>
 
                 </Row>
+
+                {/* User deails ends here */}
+
             </Col>
         </Row>
 
