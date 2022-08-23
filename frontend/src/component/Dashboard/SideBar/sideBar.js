c549cc3908d4135861094cbbb7882c3bb5ef5d4f
akilamaithri <akilamaithripala@gmail.com>
2022-08-23 09:12:17 +0530

    side bar update

--- frontend/src/component/Dashboard/SideBar/sideBar.js
+++ frontend/src/component/Dashboard/SideBar/sideBar.js
@@ -1,21 +1,64 @@
 import React, { Component } from 'react';
 import './sideBarStyles.css';
+// import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-
+import Nav from 'react-bootstrap/Nav';
+import NavLink from 'react-bootstrap/NavLink';
+import Container from "react-bootstrap/esm/Container";
 
 class SideBar extends Component {
 
+
     render() { 
+
+      const dashTitle1 = this.props.dashTitle1;
+      const dashLink1 = this.props.dashLink1;
+      
+      const dashTitle2 = this.props.dashTitle2;
+      const dashLink2 = this.props.dashLink2;
+      
+      const dashTitle3 = this.props.dashTitle3;
+      const dashLink3 = this.props.dashLink3;
+      
+      const dashTitle4 = this.props.dashTitle4;
+      const dashLink4 = this.props.dashLink4;
+      
+      const dashTitle5 = this.props.dashTitle5;
+      const dashLink5 = this.props.dashLink5;
+
+      const dashTitle6 = this.props.dashTitle6;
+      const dashLink6 = this.props.dashLink6;
+
+
         return (
 
           <div>
 
             <nav id="sidebarMenu" className="collapse d-lg-block sidebar collapse bg-white">
               <div className="position-sticky">
+
                 <div className="list-group list-group-flush mx-3 mt-4">
-                  <a href="#" className="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
-                    <span>Main dashboard</span>
+
+                  <Nav.Link className="list-group-item list-group-item-action py-2 ripple active mb-3"  as={Link} eventKey={'/'+dashLink1} to={'/'+dashLink1} >{dashTitle1}</Nav.Link>
+
+                  <Nav.Link  className="list-group-item list-group-item-action py-2 ripple active mb-3" as={Link} to={'/'+dashLink2} >{dashTitle2}</Nav.Link>
+
+                  <Nav.Link  className="list-group-item list-group-item-action py-2 ripple active mb-3" as={Link} to={'/'+dashLink3} >{dashTitle3}</Nav.Link>
+
+
+                  <Nav.Link  className="list-group-item list-group-item-action py-2 ripple active mb-3" as={Link} to={'/'+dashLink4} >{dashTitle4}</Nav.Link>
+
+                  <Nav.Link  className="list-group-item list-group-item-action py-2 ripple active mb-3" as={Link} to={'/'+dashLink5} >{dashTitle5}</Nav.Link>
+
+                  <Nav.Link  className="list-group-item list-group-item-action py-2 ripple active mb-3" as={Link} to={'/'+dashLink6} >{dashTitle6}</Nav.Link>
+
+                  {/* <a href="#" className="list-group-item list-group-item-action py-2 ripple active" aria-current="true">
+                    <span>Student Home</span>
                   </a>
+
                   <a href="#" className="list-group-item list-group-item-action py-2 ripple">
                     <span>Webiste traffic</span>
                   </a>
@@ -37,7 +80,7 @@ class SideBar extends Component {
 
                   <a href="#" className="list-group-item list-group-item-action py-2 ripple"><span>Users</span></a>
 
-                  <a href="#" className="list-group-item list-group-item-action py-2 ripple"><span>Sales</span></a>
+                  <a href="#" className="list-group-item list-group-item-action py-2 ripple"><span>Sales</span></a> */}
 
                 </div>
               </div>
