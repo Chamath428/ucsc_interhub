04f714a91e598c0f24551e9a8050379cbb35423e
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2022-09-13 18:50:59 +0530

    Reuploaded files

--- backend/controllers/staffMemberController.js
+++ backend/controllers/staffMemberController.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import {staffMemberSchema} from '../models/staffMemberModel.js';
+import {staffMemberSchema,staffMemberScheduleCompanyVisit} from '../models/staffMemberModel.js';
 import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
@@ -26,6 +26,34 @@ export const createStaffMember = async (req,res)=>{
             res.status(500).send(error);
         }
     }else{
+        res.status(500).send(error);
+    }
+}
+
+export const scheduleCompanyVisit = async (req,res)=>{
+    const{error,value}=staffMemberScheduleCompanyVisit.validate(req.body);
+
+    if(!error){
+        // const salt = await bcrypt.genSalt(10);
+        // const hashPassword = await bcrypt.hash("password", salt); 
+        try{           
+            const company_visit = await prisma.company_visit.create({
+                data:{
+                    company_id:req.body.company_id,
+                    email_address:req.body.email_address,
+                    date:req.body.date,
+                    start_time:req.body.start_time,
+                    company_visit_type:req.body.company_visit_type
+                }
+            })
+
+            res.status(200).send(company_visit);
+        }catch(error){
+            console.log(error);
+            res.status(400).send(error);
+        }
+    }else{
+        console.log(error);
         res.status(500).send(error);
     }
 }
