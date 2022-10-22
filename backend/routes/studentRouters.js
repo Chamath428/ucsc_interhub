47312eec3d9f5d78373881cd35e2b9476e034095
AnupamaBandara <2019cs009@stu.ucsc.lk>
2022-10-22 11:31:37 +0530

    Student Edit Profile Backend first half done

--- backend/routes/studentRouters.js
+++ backend/routes/studentRouters.js
@@ -1,8 +1,11 @@
 import express from 'express';
+// import StudentEditProfile from '../../frontend/src/pages/Student/StudentSettings.js';
 import {createStudent} from '../controllers/studentController.js';
+import {studentEditProfile} from '../controllers/studentController.js';
 
 const studentRouters = express.Router();
 
 studentRouters.post('/create',createStudent);
+studentRouters.post('/editProfile',studentEditProfile);
 
 export default studentRouters;
