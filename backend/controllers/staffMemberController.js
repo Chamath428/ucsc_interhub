b575a7798233503ffd963e15e683ba7179b49b58
GimhaniRubasinghe <2019cs148@stu.ucsc.lk>
2022-10-22 11:40:57 +0530

    Upload Staff Member changed files

--- backend/controllers/staffMemberController.js
+++ backend/controllers/staffMemberController.js
@@ -30,7 +30,144 @@ export const createStaffMember = async (req,res)=>{
         res.status(500).send(error);
     }
 }
+export const ActiveRegisteredCompany = async (req, res) => {
 
+    try {
+        const activeRegisteredCompany = await prisma.company.findMany({
+            where: 
+            {
+                company_status: 2 , 
+            },
+
+            select: {
+                name: true,
+                company_contacts: {
+                    select: {
+                        telephone_no: true,
+                    },
+                },
+                registration_number: true,
+            }
+        })
+        res.status(200).send(activeRegisteredCompany);
+
+        // console.dir(registeredCompany, { depth: null })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+}
+export const InactiveRegisteredCompany = async (req, res) => {
+
+    try {
+        const InactiveRegisteredCompany = await prisma.company.findMany({
+            where: {
+                
+                company_status: 3 , 
+            },
+            select: {
+                name: true,
+                company_contacts: {
+                    select: {
+                        telephone_no: true,
+                    },
+                },
+                registration_number: true,
+            }
+        })
+        res.status(200).send(InactiveRegisteredCompany);
+
+        // console.dir(registeredCompany, { depth: null })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+}
+export const PendingCompany = async (req, res) => {
+    try {
+        const PendingCompany = await prisma.company.findMany({
+            where: {
+                company_status: 1,
+
+            },
+            select: {
+                name: true,
+                company_contacts: {
+                    select: {
+                        telephone_no: true,
+                    },
+                },
+                registration_number: true,
+
+            }
+
+        })
+        res.status(200).send(PendingCompany);
+
+        // console.dir(PendingCompany, { depth: null })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+}
+export const ActiveCompany = async (req, res) => {
+
+    try {
+        const ActiveCompany = await prisma.company.findMany({
+            where: {
+                company_status: 2 ,
+            },
+            select: {
+                company_id: true,
+                name: true,
+            }
+        })
+        res.status(200).send(ActiveCompany);
+
+        // console.dir(ActiveCompany, { depth: null })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+
+}
+export const CompanyVisit = async (req, res) => {
+
+
+    try {
+        const CompanyVisit = await prisma.company_visit.findMany({
+
+            select: {
+                company: {
+                    select: {
+                        name: true,
+
+                    },
+                },
+                email_address:true,
+                date: true,
+                
+                start_time: true,
+                company_visit_types: {
+                    select: {
+                        type: true,
+                    },
+                },
+                company_visit_status_types:{
+                    select: {
+                        status:true,
+                    },
+                },
+
+
+            }
+        })
+        res.status(200).send(CompanyVisit);
+
+        // console.dir(CompanyVisit, { depth: null })
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
 export const scheduleCompanyVisit = async (req,res)=>{
     const{error,value}=staffMemberScheduleCompanyVisit.validate(req.body);
 
